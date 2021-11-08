52aadd27e29b22e7e96f87c06fd7ae96326cd1a4
Daniel Herrera <herrerad@ohsu.edu>
2021-11-08 14:24:32 -0500

    added test folder

---
+++ test/app.R
@@ -0,0 +1,191 @@
+library(shinythemes)
+library(shiny)
+library(tidyverse)
+library(ggthemes)
+library(ggrepel)
+library(gridExtra)
+
+#############################
+
+# wages dataset
+wages <- read_csv("wages_norepeats.csv")
+wages <- as_tibble(wages) %>%
+    rename(city = City, occupation = Occupation, hourly = 'Hourly Wage', annual = 'Annual Wage')
+
+# need to remove wage values labeled as > 208,000
+wages <- wages %>%
+    mutate(annual  = recode(annual,
+                            ">208000" = 'NA'
+    )) %>%
+    mutate(annual = str_remove(annual, ",")) %>% 
+    mutate(annual = as.numeric(annual))
+
+# cost of living:col dataset
+cost <- read_csv("col_citydata.csv")
+
+
+
+
+cities <- sort(unique(wages$city))
+occupations <- sort(unique(wages$occupation))
+
+
+##############################
+
+
+
+# Define UI ----
+ui <- fluidPage(theme = shinytheme("sandstone"),
+                titlePanel("Checks & Cities"),
+                
+                # create first tab 
+                tabsetPanel(
+                    tabPanel("General Salary Information",
+                             sidebarLayout(
+                                 sidebarPanel(
+                                     # add text explaining proces
+                                     
+                                    p(strong("Want to know some general information about salaries?")),
+                                    br(),
+                                    p("Input your city of interest below to get the mean salary in that city."),
+                                    p("Input your desired occupation to get the average salaries by city."),
+                                     # select input here for mycity: city you are looking at
+                                     selectInput(inputId = "mycity",
+                                                 label = "What cities are you interested in?", choices = cities),
+                                     selectInput(inputId = "myjob",
+                                                 label = "What is your occupation?", choices = occupations)),
+                                 mainPanel(
+                                     verbatimTextOutput(outputId = "citysalary"),
+                                     plotOutput(outputId = "jobsalary", height = 600))
+                             )
+                    ),
+                    
+                    tabPanel("Offer Comparison",
+                             sidebarLayout(
+                                 sidebarPanel(
+                                     numericInput(inputId = "offer1",
+                                                  label = "What is your first offer (annual salary)?", 50000, min = 1, max = 10000000),
+                                     selectInput(inputId = "city1",
+                                                 label = "What city is your first offer in?", choices = cities, selected = "Los Angeles"),
+                                     numericInput(inputId = "offer2",
+                                                  label = "What is your second offer (annual salary)?", 50000, min = 1, max = 10000000),
+                                     selectInput(inputId = "city2",
+                                                 label = "What city is your second offer in?", choices = cities, selected = "Boston")),
+                                 mainPanel(
+                                     plotOutput(outputId = "comparison", height = 500)
+                                 )
+                             )
+                    )
+                )
+)
+
+# Define server logic ----
+server <- function(input, output) {
+    
+    # Return mean salary
+    output$citysalary <- renderText({
+        mean_salary <- wages %>%
+            filter(city == input$mycity) %>%
+            filter(occupation == "All Occupations") %>%
+            select(annual)
+        paste0( "The mean salary in ", as.character(input$mycity) ," is $",  round(as.numeric(mean_salary), 2))
+    })
+    
+    # first plot of salary by occupations
+    output$jobsalary <- renderPlot(
+        wages %>%
+            filter(occupation == input$myjob) %>%
+            ggplot(., aes(x = annual, y = reorder(city, - annual), label = annual)) +
+            geom_col(stat = "identity", fill = "lightsteelblue3") +
+            labs(title = "Annual Salary") +
+            theme_bw() +
+            theme(axis.text.y = element_text(angle = 0, vjust = 0, hjust = 0),
+                  axis.title.y = element_blank(),
+                  axis.ticks.y = element_blank(),
+                  axis.title.x = element_blank(),
+                  panel.grid.major.y = element_blank(),
+                  panel.grid.minor.y = element_blank(),
+                  plot.title = element_text(face = "bold"),
+                  plot.title.position = "plot") +
+            scale_x_continuous(expand = c(0,0), labels = scales::comma)+
+            geom_text(aes(label = paste0("$",annual), x = annual + 2500), size = 2.8, fontface = "bold")
+    )
+    
+    
+    output$comparison <- renderPlot({
+        # create two tables, to ensure the offer goes in the right row with the appropriate city
+        cost1 <- cost %>%
+            filter(city == input$city1 ) %>%
+            filter(metrics == "Apartment (1 bedroom) in City Centre") %>%
+            mutate(salary = input$offer1)
+        
+        cost2 <- cost %>%
+            filter(city == input$city2 ) %>%
+            filter(metrics == "Apartment (1 bedroom) in City Centre") %>%
+            mutate(salary = input$offer2)
+        
+        # bind the comparison cities together
+        offers <- bind_rows(cost1, cost2)
+        
+        # cost of living plot via 1 bed apt cost
+        p1 <- ggplot(offers, aes(x = city, y = price, label = price)) +
+            geom_bar(stat = "identity",  fill = "lightsteelblue3") +
+            labs(title = "Cost of 1 Bedroom Apartment in City",
+                 x = "$") +
+            geom_text(aes(label = paste0("$",round(price,2)), y = price + 50), size = 3, fontface = "bold") +
+            theme_bw() +
+            theme(
+                axis.title.x = element_blank(),
+                axis.ticks.x = element_blank(),
+                panel.grid.major.x = element_blank(),
+                panel.grid.minor.x = element_blank(),
+                plot.title = element_text(face = "bold"),
+                plot.title.position = "plot")
+        
+        # salary offer plot
+        p2 <- offers %>% 
+            mutate(month_sal = (salary/365) * 30.5) %>%
+            ggplot(., aes(x = city, y = month_sal, label = month_sal)) +
+            geom_bar(stat = "identity",  fill = "lightsteelblue3") +
+            labs(title = "Monthly Salary Comparison") +
+            geom_text(aes(label = paste0("$",round(month_sal,2)), y = month_sal + 50), size = 3, fontface = "bold") +
+            theme_bw() +
+            theme(
+                axis.title.x = element_blank(),
+                axis.ticks.x = element_blank(),
+                axis.title.y = element_blank(),
+                panel.grid.major.x = element_blank(),
+                panel.grid.minor.x = element_blank(),
+                plot.title = element_text(face = "bold"),
+                plot.title.position = "plot")
+        
+        
+        # salary offer plot
+        # make salary per month -- salary divided by 356 and then by 30.5 days
+        p3 <- offers %>%
+            mutate(monthly_remainder = ((salary/365) * 30.5) - price) %>%
+            ggplot(., aes(x = city, y = monthly_remainder, label = monthly_remainder)) +
+            geom_bar(stat = "identity",  fill = "steelblue4") +
+            labs(title = "Surplus Montly Income") +
+            geom_text(aes(label = paste0("$",round(monthly_remainder,2)), y = monthly_remainder + 50), size = 3, fontface = "bold") +
+            theme_bw() +
+            theme(
+                axis.title.x = element_blank(),
+                axis.title.y = element_blank(),
+                axis.ticks.x = element_blank(),
+                panel.grid.major.x = element_blank(),
+                panel.grid.minor.x = element_blank(),
+                plot.title = element_text(face = "bold"),
+                plot.title.position = "plot")
+        
+        grid.arrange(p1,p2,p3, ncol = 3)
+        
+    }
+    )
+    
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
+
+
