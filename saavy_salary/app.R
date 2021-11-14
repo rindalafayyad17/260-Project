2d3937d308d8e2e164815ef5610c5f4bb57f46de
Daniel Herrera <herrerad@ohsu.edu>
2021-11-13 23:59:29 -0500

    final shiny app! (I think)

--- saavy_salary/app.R
+++ saavy_salary/app.R
@@ -7,11 +7,12 @@ library(gridExtra)
 
 #############################
 
-# wages dataset
+# wages/salary dataset
 wages <- read_csv("wages_norepeats.csv")
 wages <- as_tibble(wages) %>%
     rename(city = City, occupation = Occupation, hourly = 'Hourly Wage', annual = 'Annual Wage')
 
+# data cleaning
 # need to remove wage values labeled as > 208,000
 wages <- wages %>%
     mutate(annual  = recode(annual,
@@ -20,9 +21,17 @@ wages <- wages %>%
     mutate(annual = str_remove(annual, ",")) %>% 
     mutate(annual = as.numeric(annual))
 
-# cost of living:col dataset
-cost <- read_csv("col_citydata.csv")
+# cost of living data
+cost <- read_csv("full_col_data.csv")
 
+# will need to make the data wide
+# create new variable which will be used to calculate monthly expenses and discretionary income
+cost <- cost %>% 
+    pivot_wider(names_from = metrics, values_from = price) %>% 
+    # average meals per week out is 4 (16 monthly)! 
+    mutate(expenses = 16 * as.numeric(`Meal, Inexpensive Restaurant`) + as.numeric(`Apartment (1 bedroom) in City Centre`) + 
+               as.numeric(`Basic (Electricity, Heating, Cooling, Water, Garbage) for 915 sq ft Apartment`) + 
+               as.numeric(`Internet (60 Mbps or More, Unlimited Data, Cable/ADSL)`))
 
 # set the values which can be selected from the city and occupations inputs
 cities <- sort(unique(wages$city))
@@ -86,17 +95,18 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                                      # add text explaining process
                                      p(strong("Do you have two competing offers from different cities that you want to compare?")),
                                      p("Input your offers and cities below."),
-                                     p("The first plot will give you your cost of living (currently apartment cost). The second will give monthly salary. The third will give your remaining amount of money."),
-                                     p("Choose the city that maximizes plot 3!"),
+                                     p("The first plot will give you your cost of living. The second will give monthly salary. The third will give your remaining amount of money."),
+                                     p(em("Cost of living was determined by adding city averages for basic utilities, internet, one bedroom apartment in city, and 16 meals out per month.")),
+                                     p(strong("Choose the city that maximizes Monthly Discetionary Income (plot 3)!")),
                                      br(),
                                      
                                      # these are the inputs for this panel 
                                      numericInput(inputId = "offer1",
-                                                  label = "What is your first offer (annual salary)?", 50000, min = 1, max = 10000000),
+                                                  label = "What is your first offer (annual salary)?", 60000, min = 1, max = 10000000),
                                      selectInput(inputId = "city1",
                                                  label = "What city is your first offer in?", choices = cities, selected = "Los Angeles"),
                                      numericInput(inputId = "offer2",
-                                                  label = "What is your second offer (annual salary)?", 50000, min = 1, max = 10000000),
+                                                  label = "What is your second offer (annual salary)?", 65000, min = 1, max = 10000000),
                                      selectInput(inputId = "city2",
                                                  label = "What city is your second offer in?", choices = cities, selected = "Boston")),
                                  # this is the main panel output for this tab
@@ -119,7 +129,7 @@ server <- function(input, output) {
             filter(city == input$mycity) %>%
             filter(occupation == "All Occupations") %>%
             select(annual)
-        paste0( "The mean salary of all jobs in ", as.character(input$mycity) ," is $",  round(as.numeric(mean_salary), 2), ".")
+        paste0( "The mean salary of all jobs in ", as.character(input$mycity) ," is $",  prettyNum(round(as.numeric(mean_salary), 2), big.mark = ","), ".")
     })
     
     # first plot of salary by occupations
@@ -133,7 +143,7 @@ server <- function(input, output) {
         # create the ggplot    
         wages %>%
             filter(occupation == input$myjob) %>%
-            ggplot(., aes(x = annual, y = reorder(city, - annual), label = annual)) +
+            ggplot(., aes(x = annual, y = reorder(city, - annual), label = prettyNum(annual, big.mark = ","))) +
             geom_col(stat = "identity", fill = "lightsteelblue3") +
             # make the title change according to input
             labs(title = paste0("Annual Salary of ", input$myjob)) +
@@ -147,7 +157,7 @@ server <- function(input, output) {
                   plot.title = element_text(face = "bold"),
                   plot.title.position = "plot") +
             scale_x_continuous(expand = c(0,0), labels = scales::comma, limits = c(0, maxwage + 10000))+
-            geom_text(aes(label = paste0("$",annual), x = annual + 2500), size = 4, fontface = "bold")
+            geom_text(aes(label = paste0("$",prettyNum(annual, big.mark = ",")), x = annual + 2500), size = 4, fontface = "bold")
     }
     )
     
@@ -156,23 +166,23 @@ server <- function(input, output) {
         # create two tables, to ensure the offer goes in the right row with the appropriate city
         cost1 <- cost %>%
             filter(city == input$city1 ) %>%
-            filter(metrics == "Apartment (1 bedroom) in City Centre") %>%
+            select(city, expenses) %>%
             mutate(salary = input$offer1)
         
         cost2 <- cost %>%
             filter(city == input$city2 ) %>%
-            filter(metrics == "Apartment (1 bedroom) in City Centre") %>%
+            select(city, expenses) %>%
             mutate(salary = input$offer2)
         
         # bind the comparison cities together
         offers <- bind_rows(cost1, cost2)
         
         # cost of living plot via 1 bed apt cost
-        p1 <- ggplot(offers, aes(x = city, y = price, label = price)) +
+        p1 <- ggplot(offers, aes(x = city, y = expenses, label = expenses)) +
             geom_bar(stat = "identity",  fill = "lightsteelblue3") +
-            labs(title = "Cost of 1 Bedroom Apartment in City",
+            labs(title = "Monthly Expenses",
                  x = "$") +
-            geom_text(aes(label = paste0("$",round(price,2)), y = price + 50), size = 3, fontface = "bold") +
+            geom_text(aes(label = paste0("$",prettyNum(round(expenses,2), big.mark = ",")), y = expenses + 50), size = 4, fontface = "bold") +
             theme_bw() +
             theme(
                 axis.title.x = element_blank(),
@@ -188,7 +198,7 @@ server <- function(input, output) {
             ggplot(., aes(x = city, y = month_sal, label = month_sal)) +
             geom_bar(stat = "identity",  fill = "lightsteelblue3") +
             labs(title = "Monthly Salary Comparison") +
-            geom_text(aes(label = paste0("$",round(month_sal,2)), y = month_sal + 50), size = 3, fontface = "bold") +
+            geom_text(aes(label = paste0("$",prettyNum(round(month_sal,2),big.mark = ",")), y = month_sal + 50), size = 4, fontface = "bold") +
             theme_bw() +
             theme(
                 axis.title.x = element_blank(),
@@ -203,11 +213,11 @@ server <- function(input, output) {
         # salary offer plot
         # make salary per month -- salary divided by 356 and then by 30.5 days
         p3 <- offers %>%
-            mutate(monthly_remainder = ((salary/365) * 30.5) - price) %>%
+            mutate(monthly_remainder = ((salary/365) * 30.5) - expenses) %>%
             ggplot(., aes(x = city, y = monthly_remainder, label = monthly_remainder)) +
             geom_bar(stat = "identity",  fill = "steelblue4") +
-            labs(title = "Surplus Montly Income") +
-            geom_text(aes(label = paste0("$",round(monthly_remainder,2)), y = monthly_remainder + 50), size = 3, fontface = "bold") +
+            labs(title = "Discretionary Monthly Income") +
+            geom_text(aes(label = paste0("$",prettyNum(round(monthly_remainder,2), big.mark = ",")), y = monthly_remainder + 50), size = 4, fontface = "bold") +
             theme_bw() +
             theme(
                 axis.title.x = element_blank(),
@@ -227,7 +237,6 @@ server <- function(input, output) {
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
-
 
 
 
--- test/app.R
+++ test/app.R
@@ -7,11 +7,12 @@ library(gridExtra)
 
 #############################
 
-# wages dataset
+# wages/salary dataset
 wages <- read_csv("wages_norepeats.csv")
 wages <- as_tibble(wages) %>%
     rename(city = City, occupation = Occupation, hourly = 'Hourly Wage', annual = 'Annual Wage')
 
+# data cleaning
 # need to remove wage values labeled as > 208,000
 wages <- wages %>%
     mutate(annual  = recode(annual,
@@ -20,9 +21,17 @@ wages <- wages %>%
     mutate(annual = str_remove(annual, ",")) %>% 
     mutate(annual = as.numeric(annual))
 
-# cost of living:col dataset
-cost <- read_csv("col_citydata.csv")
+# cost of living data
+cost <- read_csv("full_col_data.csv")
 
+# will need to make the data wide
+# create new variable which will be used to calculate monthly expenses and discretionary income
+cost <- cost %>% 
+    pivot_wider(names_from = metrics, values_from = price) %>% 
+    # average meals per week out is 4 (16 monthly)! 
+    mutate(expenses = 16 * as.numeric(`Meal, Inexpensive Restaurant`) + as.numeric(`Apartment (1 bedroom) in City Centre`) + 
+               as.numeric(`Basic (Electricity, Heating, Cooling, Water, Garbage) for 915 sq ft Apartment`) + 
+               as.numeric(`Internet (60 Mbps or More, Unlimited Data, Cable/ADSL)`))
 
 # set the values which can be selected from the city and occupations inputs
 cities <- sort(unique(wages$city))
@@ -86,17 +95,18 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                                      # add text explaining process
                                      p(strong("Do you have two competing offers from different cities that you want to compare?")),
                                      p("Input your offers and cities below."),
-                                     p("The first plot will give you your cost of living (currently apartment cost). The second will give monthly salary. The third will give your remaining amount of money."),
-                                     p("Choose the city that maximizes plot 3!"),
+                                     p("The first plot will give you your cost of living. The second will give monthly salary. The third will give your remaining amount of money."),
+                                     p(em("Cost of living was determined by adding city averages for basic utilities, internet, one bedroom apartment in city, and 16 meals out per month.")),
+                                     p(strong("Choose the city that maximizes Monthly Discetionary Income (plot 3)!")),
                                      br(),
                                      
                                      # these are the inputs for this panel 
                                      numericInput(inputId = "offer1",
-                                                  label = "What is your first offer (annual salary)?", 50000, min = 1, max = 10000000),
+                                                  label = "What is your first offer (annual salary)?", 60000, min = 1, max = 10000000),
                                      selectInput(inputId = "city1",
                                                  label = "What city is your first offer in?", choices = cities, selected = "Los Angeles"),
                                      numericInput(inputId = "offer2",
-                                                  label = "What is your second offer (annual salary)?", 50000, min = 1, max = 10000000),
+                                                  label = "What is your second offer (annual salary)?", 65000, min = 1, max = 10000000),
                                      selectInput(inputId = "city2",
                                                  label = "What city is your second offer in?", choices = cities, selected = "Boston")),
                                  # this is the main panel output for this tab
@@ -156,23 +166,23 @@ server <- function(input, output) {
         # create two tables, to ensure the offer goes in the right row with the appropriate city
         cost1 <- cost %>%
             filter(city == input$city1 ) %>%
-            filter(metrics == "Apartment (1 bedroom) in City Centre") %>%
+            select(city, expenses) %>%
             mutate(salary = input$offer1)
         
         cost2 <- cost %>%
             filter(city == input$city2 ) %>%
-            filter(metrics == "Apartment (1 bedroom) in City Centre") %>%
+            select(city, expenses) %>%
             mutate(salary = input$offer2)
         
         # bind the comparison cities together
         offers <- bind_rows(cost1, cost2)
         
         # cost of living plot via 1 bed apt cost
-        p1 <- ggplot(offers, aes(x = city, y = price, label = price)) +
+        p1 <- ggplot(offers, aes(x = city, y = expenses, label = expenses)) +
             geom_bar(stat = "identity",  fill = "lightsteelblue3") +
-            labs(title = "Cost of 1 Bedroom Apartment in City",
+            labs(title = "Monthly Expenses",
                  x = "$") +
-            geom_text(aes(label = paste0("$",round(price,2)), y = price + 50), size = 3, fontface = "bold") +
+            geom_text(aes(label = paste0("$",round(expenses,2)), y = expenses + 50), size = 4, fontface = "bold") +
             theme_bw() +
             theme(
                 axis.title.x = element_blank(),
@@ -188,7 +198,7 @@ server <- function(input, output) {
             ggplot(., aes(x = city, y = month_sal, label = month_sal)) +
             geom_bar(stat = "identity",  fill = "lightsteelblue3") +
             labs(title = "Monthly Salary Comparison") +
-            geom_text(aes(label = paste0("$",round(month_sal,2)), y = month_sal + 50), size = 3, fontface = "bold") +
+            geom_text(aes(label = paste0("$",round(month_sal,2)), y = month_sal + 50), size = 4, fontface = "bold") +
             theme_bw() +
             theme(
                 axis.title.x = element_blank(),
@@ -203,11 +213,11 @@ server <- function(input, output) {
         # salary offer plot
         # make salary per month -- salary divided by 356 and then by 30.5 days
         p3 <- offers %>%
-            mutate(monthly_remainder = ((salary/365) * 30.5) - price) %>%
+            mutate(monthly_remainder = ((salary/365) * 30.5) - expenses) %>%
             ggplot(., aes(x = city, y = monthly_remainder, label = monthly_remainder)) +
             geom_bar(stat = "identity",  fill = "steelblue4") +
-            labs(title = "Surplus Montly Income") +
-            geom_text(aes(label = paste0("$",round(monthly_remainder,2)), y = monthly_remainder + 50), size = 3, fontface = "bold") +
+            labs(title = "Discretionary Monthly Income") +
+            geom_text(aes(label = paste0("$",round(monthly_remainder,2)), y = monthly_remainder + 50), size = 4, fontface = "bold") +
             theme_bw() +
             theme(
                 axis.title.x = element_blank(),
@@ -227,7 +237,6 @@ server <- function(input, output) {
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
-
 
 
 
